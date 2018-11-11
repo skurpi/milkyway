5f1ed21372bedd52ffcda9bd08c1f9c1b422ea98
Skurpi <dario.vodopivec@gmail.com>
2018-11-11 14:27:44 +0100

    Basic layout of the app design

--- App.js
+++ App.js
@@ -1,22 +1,48 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, Text, TextInput, View, SafeAreaView, Platform } from 'react-native';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Or should you!?</Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <SafeAreaView style={style.droidSafeArea}>
+                <View style={{ flex: 1, alignItems: 'center' }}>
+                    <Text>Boobtracker</Text>
+                    <Text>Latest feeds</Text>
+                    <View style={{ flex: 1, flexDirection: 'row' }}>
+                        <View style={{ flex: 1, backgroundColor: 'powderblue' }} >
+                            <Text>Monday</Text>
+                        </View>
+                        <View style={{ flex: 1, backgroundColor: 'skyblue' }} >
+                            <Text>Sunday</Text>
+                        </View>
+                        <View style={{ flex: 1, backgroundColor: 'steelblue' }} >
+                            <Text>Saturday</Text>
+                        </View>
+                    </View>
+                    <Text>Add feed</Text>
+                    <View style={{ flex: 1, alignItems: 'center' }}>
+                        <Text>TimePicker</Text>{/*https://reactnativeexample.com/a-react-native-datetime-picker-for-android-and-ios/ */}
+                        <TextInput
+                            style={{ height: 40 }}
+                            placeholder="Extra notes?"
+                            onChangeText={(text) => this.setState({ text })}
+                        />
+                        <Button
+                            onPress={() => { console.log('todo') }}
+                            title="Save"
+                            color="#841584"
+                            accessibilityLabel="Save the details of the feed"
+                        />
+                    </View>
+                </View>
+            </SafeAreaView>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const style = StyleSheet.create({
+    droidSafeArea: {
+        flex: 1,
+        paddingTop: Platform.OS === 'android' ? 25 : 0
+    },
+})
