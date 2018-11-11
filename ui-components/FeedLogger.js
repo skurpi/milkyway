941a736dad3571b67d6df49726dcfc1197422216
Skurpi <dario.vodopivec@gmail.com>
2018-11-11 17:17:23 +0100

    Add timepicker and handle basic selecting of time

--- ui-components/FeedLogger.js
+++ ui-components/FeedLogger.js
@@ -1,16 +1,41 @@
 import React, { Component } from "react";
-import { View, Text, TextInput, Button } from "react-native";
+import { View, TextInput, Button, Text } from "react-native";
+import DateTimePicker from "react-native-modal-datetime-picker";
 
 import Header from "./Header";
 
+function handleDate(date) {
+  return typeof date === "string" ? date : date.toString();
+}
+
 export default class FeedLogger extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isDateTimePickerVisible: false, date: "No date selected" };
+
+    this.showTimePicker = () =>
+      this.setState({ isDateTimePickerVisible: true });
+
+    this.hideTimePicker = () =>
+      this.setState({ isDateTimePickerVisible: false });
+
+    this.handleDatePicked = date => {
+      this.setState({ date: date.toString(), isDateTimePickerVisible: false });
+    };
+  }
+
   render() {
     return (
       <>
         <Header>Add feed</Header>
         <View style={{ flex: 1, alignItems: "center" }}>
-          <Text>TimePicker</Text>
-          {/*https://reactnativeexample.com/a-react-native-datetime-picker-for-android-and-ios/ */}
+          <Text>{handleDate(this.state.date)}</Text>
+          <Button
+            onPress={this.showTimePicker}
+            title="Started feeding"
+            color="#841584"
+            accessibilityLabel="Choose the time that the feeding started"
+          />
           <TextInput
             style={{ height: 40 }}
             placeholder="Extra notes?"
@@ -23,6 +48,12 @@ export default class FeedLogger extends Component {
             accessibilityLabel="Save the details of the feed"
           />
         </View>
+        <DateTimePicker
+          mode={"time"}
+          isVisible={this.state.isDateTimePickerVisible}
+          onConfirm={this.handleDatePicked}
+          onCancel={this.hideTimePicker}
+        />
       </>
     );
   }
